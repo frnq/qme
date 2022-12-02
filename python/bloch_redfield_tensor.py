e3194d0d81078076e45529455039a8cb7783011b
frnq <79286633+frnq@users.noreply.github.com>
2022-12-02 16:12:50 +1100

    Update bloch_redfield_tensor.py

--- python/bloch_redfield_tensor.py
+++ python/bloch_redfield_tensor.py
@@ -1,12 +1,42 @@
 import numpy as np
-from qutip import *
+
+def BR_tensor(H,a_ops,secular=True,secular_cut_off = 0.01):
+    dim = len(H) # dimension
+    evals,ekets = np.linalg.eig(H) # HS's basis
+    # sort basis
+    _zipped = list(zip(evals, range(len(evals))))
+    _zipped.sort()
+    evals, perm = list(zip(*_zipped))
+    ekets = np.array([ekets[:, k] for k in perm])
+    evals = np.array(evals)
+    a_ops_S = [[np.linalg.inv(ekets)@a@np.linalg.inv(ekets),nps] for a,nps in a_ops] # coupling ops in ekets basis
+    # Bohr frequencies (w_ab)
+    indices = [(a,b) for a in range(dim) for b in range(dim)]
+    BohrF = np.sort(np.array([evals[a]-evals[b] for a in range(dim) for b in range(dim)]))
+    R = np.zeros((dim**2,dim**2),dtype = complex) # construct empty R
+    for j,(a,b) in enumerate(indices): # loop over indices
+        for k,(c,d) in enumerate(indices): # loop over indices
+            # unitary part
+            R[j,k] += -1j * (a==c)*(b==d)*(evals[a]-evals[b])
+            for a_op,nps in a_ops_S: # loop over uncorrelated a_ops
+                gmax = np.max([NPS(f) for f in BohrF]) # largest rate for secular approximation
+                A = a_op # coupling operator
+                # secular approximation test
+                if secular is True and abs(evals[a]-evals[b]-evals[c]+evals[d]) > gmax*secular_cut_off:
+                    pass
+                else:      
+                    # non-unitary part
+                    R[j,k] += - 1/2 * ((b==d)*np.sum([A[a,n]*A[n,c]*nps(evals[c]-evals[n])  
+                                                      for n in range(dim)])
+                                                     -A[a,c]*A[d,b]*nps(evals[c]-evals[a]) +
+                                       (a==c)*np.sum([A[d,n]*A[n,b]*nps(evals[d]-evals[n])  
+                                                      for n in range(dim)])
+                                                     -A[a,c]*A[d,b]*nps(evals[d]-evals[b]))
+    return R
 
 e0, delta = 1,0.2 # spin parameters
-HS = e0/2 * sigmaz() + delta/2 * sigmax() # spin Hamiltonian
-dim = len(HS.full()) # dimension
-evals,ekets = HS.eigenstates() # HS's basis
-# Bohr frequencies (w_ab)
-BohrF = np.sort(np.array([evals[a]-evals[b] for a in range(dim) for b in range(dim)]))
+sz,sx = np.array([[1,0],[0,-1]]), np.array([[0,1],[1,0]])
+HS = e0/2 * sz + delta/2 * sx # spin Hamiltonian
 
 def S(w,wc,eta,beta,thresh = 1e-10): # Noise Power Spectum
     return (2*np.pi*eta*w*np.exp(-abs(w)/wc) /
@@ -15,29 +45,6 @@ def S(w,wc,eta,beta,thresh = 1e-10): # Noise Power Spectum
 
 # noise power spectrum
 NPS = lambda w: S(w,wc=1,eta=1,beta=2)
-a_ops = [[sigmaz(), NPS]] # coupling operator and associated NPS
-a_ops_S = [[a.transform(ekets),nps] for a,nps in a_ops] # coupling ops in ekets basis
+a_ops = [[sz, NPS]] # coupling operator and associated NPS
 
-# secular approximation
-secular = True
-secular_cut_off = 0.01
-
-R = np.zeros((dim**2,dim**2),dtype = complex) # construct empty R
-for j,(a,b) in enumerate(indices): # loop over indices
-    for k,(c,d) in enumerate(indices): # loop over indices
-        # unitary part
-        R[j,k] += -1j * (a==c)*(b==d)*(evals[a]-evals[b])
-        for a_op,nps in a_ops_S: # loop over uncorrelated a_ops
-            gmax = np.max([NPS(f) for f in BohrF]) # largest rate for secular approximation
-            A = a_op.full() # coupling operator
-            # secular approximation test
-            if secular is True and abs(evals[a]-evals[b]-evals[c]+evals[d]) > gmax*secular_cut_off:
-                pass
-            else:      
-                # non-unitary part
-                R[j,k] += - 1/2 * ((b==d)*np.sum([A[a,n]*A[n,c]*nps(evals[c]-evals[n])  
-                                                  for n in range(dim)])
-                                                 -A[a,c]*A[d,b]*nps(evals[c]-evals[a]) +
-                                   (a==c)*np.sum([A[d,n]*A[n,b]*nps(evals[d]-evals[n])  
-                                                  for n in range(dim)])
-                                                 -A[a,c]*A[d,b]*nps(evals[d]-evals[b]))
+BR_tensor(HS,a_ops)
