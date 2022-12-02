66764567ff667305a6b7c0eddef6bf4c084763a7
frnq <79286633+frnq@users.noreply.github.com>
2022-12-02 15:18:28 +1100

    Update suzuki.py

--- python/suzuki.py
+++ python/suzuki.py
@@ -1,11 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.linalg import expm
-from qutip import *
 
-superop_1 = liouvillian(sigmax(),c_ops =[sigmap()]) # superoperator 1 using QuTiP
-superop_2 = liouvillian(sigmay(),c_ops =[sigmam()]) # superoperator 2 using QuTiP
-rho0 = ket2dm(basis(2,0)) # initial state
+sx = np.array([[0,1],[1,0]]) # sigma_x
+sy = np.array([[0,-1j],[1j,0]]) # sigma_y
+sp = (sx+1j*sy)/2 # sigma_plus
+sm = (sx-1j*sy)/2 # sigma_minus
+
+superop_1 = Liouvillian(sx,[sp]) # superoperator 1 
+superop_2 = Liouvillian(sy,[sm]) # superoperator 2 
+rho0 = np.array([[1,0],[0,0]]) # initial state
 
 # plot
 fig, ax = plt.subplots()
@@ -14,26 +18,27 @@ alpha = [0.2,0.5,1]
 # number of time steps m for three possible values of m = 50,100 and 1000
 for km,m in enumerate([50,100,1000]):
     dt = 10/m # time-step
-    d = len(rho0.full()) # dimension or the state
-    P_1, P_2 = expm(superop_1.full() * dt), expm(superop_2.full() * dt) # propgators
+    d = len(rho0) # dimension or the state
+    P_1, P_2 = expm(superop_1 * dt), expm(superop_2 * dt) # propgators
     P = P_1.dot(P_2) # suzuki-trotter expansion for small times dt
     times_0, pops_0 = [], [] # allocate sets
     t, rho = 0., rho0 # initialise
     # propagate
     for k in range(m):
-        pops_0.append( (rho * ket2dm(basis(2,0)) ).tr() ) # append current population
+        pops_0.append(np.trace(rho * rho0)) # append current population
         times_0.append(t) # append current time
         # propagate time and state
-        t, rho = t+dt, Qobj(np.reshape(P.dot(np.reshape(rho.full(),(d**2,1))),(d,d)))
+        t, rho = t+dt, np.reshape(P.dot(np.reshape(rho,(d**2,1))),(d,d))
     # plot    
     ax.plot(times_0, pops_0, 'b-', label = 'w/ ST @ m = '+str(m), alpha = alpha[km]);
 
 # Hamiltonian and Lindblad operators for exact solution
-H, c_ops = sigmax()+sigmay(), [sigmap(),sigmam()]
+H, c_ops = sx+sy, [sp,sm]
 # time steps
 times_1 = np.linspace(times_0[0],times_0[-1],20)
-# usig mesolve
-pops_1 = mesolve(H,rho0,times_1,c_ops = c_ops, e_ops = [ket2dm(basis(2,0))]).expect[0] 
 # plot
-ax.plot(times_1, pops_1, 'k.', label = 'exact w/ mesolve', alpha = 0.5);
 ax.legend();
+ax.set_ylabel(r'Population $\rho_{00}(t)$')
+ax.set_xlabel(r't')
+ax.set_aspect(7);
+fig.savefig('figures/suzuki_trotter.png',transparent=True, dpi = 600,bbox_inches='tight')
