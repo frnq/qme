df317ea3039c9796e00e0d4c253d5d5615113c50
frnq <79286633+frnq@users.noreply.github.com>
2022-12-05 11:09:31 +1100

    Update random_quantum_network.py

--- python/random_quantum_network.py
+++ python/random_quantum_network.py
@@ -1,13 +1,12 @@
 import numpy as np
 from scipy.linalg import expm
 import matplotlib.pyplot as plt
-from qutip import *
 
 # constants and units
 fs = (2.4189e-2)**-1  # femtosecond in Hartree AU
 eV = ( fs * 0.6582 )**-1  #electronvolt in Hartree AU
 
-np.random.seed(1) # fix random seed
+np.random.seed(0) # fix random seed
 N = 10 # number sites
 sigma_E, sigma_V = 100e-3 * eV, 50e-3 * eV # disorder parameters
 
@@ -20,10 +19,19 @@ pairs = [all_pairs[k] for k in indices]
 Vs = [(i,j,np.random.normal(0,sigma_V)) for i,j in pairs]
 
 # Hamiltonian
-H = (Qobj(np.diag(Es)) 
-     + sum([V*basis(N,i)*basis(N,j).dag() 
-            + np.conjugate(V)*basis(N,j)*basis(N,i).dag() 
+H = (np.diag(Es)
+     + sum([V*np.outer(np.eye(N)[i],np.eye(N)[j])
+            + np.conjugate(V)*np.outer(np.eye(N)[j],np.eye(N)[i]) 
             for i,j,V in Vs]))
+
+# Hamiltonian eigendecomposition
+evals,ekets = np.linalg.eig(H) # HS's basis
+# sort basis
+_zipped = list(zip(evals, range(len(evals))))
+_zipped.sort()
+evals, perm = list(zip(*_zipped))
+ekets = np.array([ekets[:, k] for k in perm])
+evals = np.array(evals)
 
 def S(w,wc,eta,beta,thresh = 1e-10): # Noise Power Spectum
     return (2*np.pi*eta*w*np.exp(-abs(w)/wc) /
@@ -33,22 +41,25 @@ def S(w,wc,eta,beta,thresh = 1e-10): # Noise Power Spectum
 # function for NPS(w)
 NPS = lambda w: S(w,wc=150e-3*eV,eta=1e-1,beta=(25e-3*eV)**-1)
 
-a_ops = [[ket2dm(basis(N,k)), NPS]  for k in range(N)] # coupling operators
-R, ekets = bloch_redfield_tensor(H, a_ops) # Bloch-Redfield tensor
-X = Qobj(np.diag([k for k in range(N)])) # "position" operator
-X_vec = operator_to_vector(X.transform(ekets))
-rho0 = ket2dm(basis(N,0)).unit() # initial state
-rho_vec = operator_to_vector(rho0.transform(ekets))
+# coupling operators and associated noise power spectra
+a_ops = [[np.diag([float(i == k) for i in range(N)]), NPS] for k in range(N)]
+
+# Bloch-Redfield tensor in H basis
+R = BR_tensor(H, a_ops)
+
+# position operator
+X = np.diag([k for k in range(N)])
+X_vec = np.reshape(ekets.conjugate()@X@ekets.T,(1,N**2))
+rho0 = np.array([[float(i==0 and j==i) for i in range(N)] for j in range(N)])# initial state
+rho_vec = (np.reshape(ekets.conjugate()@rho0@ekets.T,(1,N**2))).T
+
 dts = [1e-1*fs,1e1*fs,1e3*fs] # adaptive time-scales
 t,tf = 0,1e6*fs
 times, pos = [], []
 for dt in dts:
-    P = Qobj(expm(R.full()*dt), dims = R.dims)
+    P = expm(R*dt)
     for m in range(1000):
         times.append(t) # append time 
-        pos.append( (rho_vec.dag()*X_vec).tr() ) # append position
-        rho_vec = P*rho_vec # propagate state
+        pos.append( np.real(np.trace((rho_vec.conjugate()@X_vec))) ) # append position
+        rho_vec = P@rho_vec # propagate state
         t += dt # propagate time
-# plot
-plt.plot(np.array(times)/fs, np.array(pos), 'k-')
-plt.xscale('log')
