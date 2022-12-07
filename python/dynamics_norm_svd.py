69d25027d63e605c6cf1a856bcd37cde3d63e5d2
frnq <79286633+frnq@users.noreply.github.com>
2022-12-07 11:24:03 +1100

    Update dynamics_norm_svd.py

--- python/dynamics_norm_svd.py
+++ python/dynamics_norm_svd.py
@@ -1,17 +1,6 @@
 import numpy as np
-from numpy import kron
 from scipy.linalg import eig
 import matplotlib.pyplot as plt
-
-# function to generate the Liouville superoperator
-def Liouvillian(H, Ls, hbar = 1):
-    d = len(H) # dimension of the system
-    superH = -1j/hbar * ( np.kron(np.eye(d),H)-np.kron(H.T,np.eye(d)) ) # Hamiltonian part
-    superL = sum([np.kron(L.conjugate(),L) 
-                  - 1/2 * ( np.kron(np.eye(d),L.conjugate().T.dot(L)) +
-                            np.kron(L.T.dot(L.conjugate()),np.eye(d)) 
-                          ) for L in Ls])
-    return superH + superL
 
 # parameters
 Omega = 0.05
@@ -26,10 +15,10 @@ superop = Liouvillian(H_s,[L])
 D,left,right = eig(superop, left=True)
 
 # initial state in vectorised form
-vec_rho_0 = np.array([0,0,0,1]); 
+vec_rho_0 = np.array([0,0,0,1], dtype = complex); 
 
 # solution
-ts = np.linspace(0, 200, 50); 
+ts = np.linspace(0, 200, 200); 
 vec_rho_t = np.zeros((len(vec_rho_0), len(ts)))
 
 for k in range(len(H_s)**2):
@@ -40,9 +29,7 @@ for k in range(len(H_s)**2):
     vec_rho_t = vec_rho_t + ak*np.array([right_k_norm*np.exp(D[k]*t) for t in ts]).T
 
 # Populations dynamics
-fig, ax = plt.subplots()
-ax.plot(ts*Omega, np.real(vec_rho_t[0]), 'k.-', label = r'$\rho_{00}(t)$' );
-ax.plot(ts*Omega, np.real(vec_rho_t[3]), 'r.-', label = r'$\rho_{11}(t)$' );
-ax.set_xlabel(r'time ($t\Omega$)')
-ax.set_ylabel(r'population')
-ax.legend();
+fig, ax = plt.subplots( figsize = (6,2))
+ax.plot(ts*Omega, np.real(vec_rho_t[3]), 'k-')
+ax.set_xlabel(r'time ($t\Omega$)', usetex = True, fontsize = 10)
+ax.set_ylabel(r'$\mathrm{Tr}[\rho(t)\rho_0]$', usetex=True, fontsize = 10);
