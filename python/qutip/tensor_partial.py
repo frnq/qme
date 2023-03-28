fd66eedb9d5e98ba7186edcd4d629ced1026fcca
frnq <79286633+frnq@users.noreply.github.com>
2023-03-28 11:43:32 +0200

    Update tensor_partial.py

--- python/qutip/tensor_partial.py
+++ python/qutip/tensor_partial.py
@@ -2,4 +2,4 @@ from qutip import *
 
 psi = (tensor(basis(2,0),basis(2,0)) + tensor(basis(2,1),basis(2,1))).unit() # normalised Bell state
 rho = psi * psi.dag() # or rho = ket2dm(psi)
-rho_1, rho_2 = rho.ptrace(1), rho.ptrace(0) # marginal states using partial trace
+rho_1, rho_2 = rho.ptrace(0), rho.ptrace(1) # marginal states using partial trace
