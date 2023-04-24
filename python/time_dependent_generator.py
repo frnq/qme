a38cb372f70c85a6700fda8b40c7d6f3ffebeaca
frnq <79286633+frnq@users.noreply.github.com>
2023-04-24 12:21:37 +0200

    Update time_dependent_generator.py

--- python/time_dependent_generator.py
+++ python/time_dependent_generator.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.linalg import expm
 
-omega0,omega,gamma = 1,3,0.3 # system parameters
+omega0,omega,gamma = 2,3,0.3 # system parameters
 H0 = omega0*np.array([[1,0],[0,-1]])/2 # Hamiltonian H0
 H1 = omega0*np.array([[0,1],[1,0]])/2 # Hamiltonian H1
 c_ops = [np.sqrt(gamma)*np.array([[0,0],[0,1]])] # Lindblad operator
@@ -50,12 +50,12 @@ times = np.linspace(0,tf,200)
 # plot
 fig, (ax,av) = plt.subplots(2,1,figsize = (6,3),gridspec_kw={'height_ratios':[2,1]})
 fig.subplots_adjust(hspace=0.1)
-ax.plot(*data_hi, 'g-', alpha = 1, label = 'sample = '+str(len(data_hi[0])));
-ax.plot(*data_mid, 'y.--', alpha = 0.7, label = 'sample = '+str(len(data_mid[0])));
-ax.plot(*data_low, 'r.--', alpha = 0.5, label = 'sample = '+str(len(data_low[0])));
+ax.plot(*data_hi, 'g-', alpha = 1, label = r'$\delta t$ = '+str(tf/len(data_hi[0])));
+ax.plot(*data_mid, 'y.--', alpha = 0.7, label = r'$\delta t$ = '+str(tf/len(data_mid[0])));
+ax.plot(*data_low, 'r.--', alpha = 0.5, label = r'$\delta t$ = '+str(tf/len(data_low[0])));
 ax.set_ylabel(r'$\mathrm{Tr}[\rho(t)\rho_0]$', usetex=True, fontsize = 10)
 ax.set_xticks([])
 av.set_xlabel(r'$t$', usetex = True, fontsize = 10)
 av.plot(times,v(times),'k-');
 av.set_ylabel(r'$v(t)$', usetex = True,fontsize = 10);
-ax.legend()
+ax.legend();
