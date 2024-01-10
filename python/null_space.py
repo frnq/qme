c8dc2f0f7c49f4b41c2bc7cb4a79f8aae689fbd6
frnq <79286633+frnq@users.noreply.github.com>
2024-01-10 15:15:49 +0100

    Update null_space.py
    Comments update for consistency

--- python/null_space.py
+++ python/null_space.py
@@ -8,7 +8,7 @@ def H_tls(omega, delta):
 def Ls_tls(g_relax, g_deph):
     return [np.sqrt(g_relax) * np.array([[0,1],[0,0]]), np.sqrt(g_deph) * np.array([[0,0],[0,1]])]
 
-# ------- (i) relaxation with no-driving --------
+# ------- (i) relaxation with driving and no dephasing --------
 omega, delta, g_relax, g_deph = 1, 1, 1, 0
 superop = Liouvillian(H_tls(omega,delta),Ls_tls(g_relax,g_deph)) # Liouville superoperator
 null = null_space(superop)
@@ -16,7 +16,7 @@ print('(i) The steady-state space is a linear subpace of dimension = '+str(len(n
 rho_ss = np.reshape(null, (2,2) )
 rho_ss /= np.trace(rho_ss)
 
-# ------- (ii) dephasing with driving --------
+# ------- (ii) dephasing with no driving --------
 omega, delta, g_relax, g_deph = 0, 1, 0, 1
 superop = Liouvillian(H_tls(omega,delta),Ls_tls(g_relax,g_deph)) # Liouville superoperator
 null = null_space(superop)
